c8ebcfe0bc62616f46d840e92bad9e361708edca
Kostiantyn Yaremchuk <w.kostiantyn.yaremchuk@gmail.com>
2025-11-06 19:29:08 +0200

    Fix: Update gitignore and summarizer logic

--- package/pipelines/video_summary.py
+++ package/pipelines/video_summary.py
@@ -2,6 +2,7 @@ import os
 from services.video_processor import downl_extract_audio
 from services.transcriber import transcribe_audio
 from services.rag_retriever import build_chunks_index
+from utils.file_utils import remove_file
 
 def run_video_processing_pipeline(youtube_URL: str):
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -22,4 +23,6 @@ def run_video_processing_pipeline(youtube_URL: str):
     except Exception as e:
         print("Error while executing pipeline.")
         return None, None
-        
+    finally:
+        remove_file(audio_path)
+     
